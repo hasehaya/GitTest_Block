6947371740fe8be407a0087af35fc149d08b53c6
hasehaya <runcona8@gmail.com>
2024-01-27 14:43:46 +0900

    Paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
-public class Paddle : MonoBehaviour
+public class Paddle :MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    BoxCollider boxCollider;
+    float width;
+    float mousePositionX;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+        width = boxCollider.size.x;
+    }
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            mousePositionX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+            if (IsTouchingWall())
+            {
+                return;
+            }
+            transform.position = new Vector3(mousePositionX, transform.position.y, transform.position.z);
+        }
+    }
+
+    bool IsTouchingLeft()
+    {
+        return mousePositionX - width / 2 <= -2;
+    }
+
+    bool IsTouchingRight()
+    {
+        return mousePositionX + width / 2 >= 2;
+    }
+
+    bool IsTouchingWall()
+    {
+        return IsTouchingLeft() || IsTouchingRight();
     }
 }
