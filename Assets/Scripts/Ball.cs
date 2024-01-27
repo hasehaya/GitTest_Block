0d78f115729a63594fbea2d0d2ad193b822d48be
hasehaya <runcona8@gmail.com>
2024-01-27 15:36:16 +0900

    ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+public class Ball :MonoBehaviour
 {
-    // Start is called before the first frame update
+    float speed = 2f;
+    Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(speed, speed, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
